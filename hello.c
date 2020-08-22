6eba267bbcb819d347fe99fd9a9e048c861420ac
nbhabish <bhabishya.neupane@huskers.unl.edu>
2020-08-22 13:44:04 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Bhabishya Neupane
+ * Date: 2020/08/17
+ *
+ * A simple hello world program
+ *
+ */
+
+#include <stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Bhabishya Neupane!\n");
+
+    return 0;
+
+}
